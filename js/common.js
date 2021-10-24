91f70b0a3b3af5a45205d431bbbecaf2df8e9853
luna <ouyanghkx@gmail.com>
2021-10-24 15:02:24 -0500

    upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 "use strict";
+const cursor = document.querySelector('.cursor');
 
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+        document.addEventListener('click', () => {
+            cursor.classList.add("expand");
+
+            setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
+        
 function doBackToTop() {
     var backToTop = {
         element: $('.go-top'),
