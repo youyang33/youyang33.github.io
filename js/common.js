91a5eb1b6f1ac1a2d3f6e5fa6e50a8a01820660f
youyang33 <ouyanghkx@gmail.com>
2023-11-26 19:31:03 -0500

    update
    dsdf

--- js/common.js
+++ js/common.js
@@ -132,7 +132,21 @@ function DoFakeAjaxPostGrid(){
     }
     
 }
+var password = "hellothere";
 
+function passcheck() {
+
+if (document.getElementById('pass1').value != password ){
+    alert ('please try agian');
+    return false;
+    
+}
+
+if (document.getElementById('pass1').value == password ){
+    return true
+}
+
+}
 /////////////////////////////////////////////
 // MOBILE MENU
 /////////////////////////////////////////////
@@ -548,3 +562,4 @@ jQuery(document).ready( function() {
     }
     showPalette();
 });
+
